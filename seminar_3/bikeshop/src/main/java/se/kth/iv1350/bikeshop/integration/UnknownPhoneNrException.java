f005aa4f39300453759571140a2806aa328d5ddd
Zainab Noah <zainabn@kth.se>
2026-05-17 12:01:31 +0200

    Exception class created

---
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/integration/UnknownPhoneNrException.java
@@ -0,0 +1,7 @@
+package se.kth.iv1350.bikeshop.integration;
+
+public class UnknownPhoneNrException extends Exception{
+    public UnknownPhoneNrException(){
+        System.out.println("The phone number does not exist in the system");
+    }
+}
