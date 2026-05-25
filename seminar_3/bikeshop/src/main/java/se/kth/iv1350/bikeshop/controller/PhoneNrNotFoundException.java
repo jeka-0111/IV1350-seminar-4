52e719dc7a855a111e577f442d5600be1e4e59a0
Zainab Noah <zainabn@kth.se>
2026-05-25 13:48:30 +0200

    Added a javadoc comment. Not sure though if it is complete.

--- seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/controller/PhoneNrNotFoundException.java
+++ seminar_3/bikeshop/src/main/java/se/kth/iv1350/bikeshop/controller/PhoneNrNotFoundException.java
@@ -1,5 +1,8 @@
 package se.kth.iv1350.bikeshop.controller;
 
+/**
+ * Exception class handling when phone number is missing. Extends the superclass Exception. 
+ */
 public class PhoneNrNotFoundException extends Exception{
     public PhoneNrNotFoundException(){
         super("Message to user: The searched phone number is not found");
